a0874e90a33085609fdcfcf9e3d7abfc1449b357
Júlia Silvestre <juliasilvestreribeiro2004@gmail.com>
2023-08-04 20:56:06 -0300

    aprendendo tipos de dados

---
+++ aula 02.py
@@ -0,0 +1,20 @@
+# first_name="julia"
+# last_name="silvestre"
+# full_name=first_name + " " + last_name
+# print(full_name)
+# print(type(first_name))
+# age = 19
+# print(age)
+# print(type(age))
+# age += 3
+# print(age)
+age = 10
+print(age)
+age += 5
+print(age)
+age += 2
+print(age)
+age = 26
+print(age)
+age2 = 41
+print(age)
